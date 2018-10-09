89e3f11ab27800f3879e63b85489a69dba3da1a9
Eliott Salazar <eliottsalazar@gmail.com>
2018-10-09 15:06:08 -0700

    .

--- ORM/ORM/Form1.Designer.cs
+++ ORM/ORM/Form1.Designer.cs
@@ -29,13 +29,20 @@
         private void InitializeComponent()
         {
             this.pnlTitulo = new System.Windows.Forms.Panel();
-            this.lblTitulo = new System.Windows.Forms.Label();
             this.pbxCerrar = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.pnlContenido = new System.Windows.Forms.Panel();
-            this.cbxServer = new System.Windows.Forms.ComboBox();
-            this.lblServer = new System.Windows.Forms.Label();
-            this.lblUser = new System.Windows.Forms.Label();
             this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.cbxServer = new System.Windows.Forms.ComboBox();
+            this.lblPuerto = new System.Windows.Forms.Label();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.lblDatabase = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
             this.pnlTitulo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCerrar)).BeginInit();
             this.pnlContenido.SuspendLayout();
@@ -43,7 +50,7 @@
             // 
             // pnlTitulo
             // 
-            this.pnlTitulo.BackColor = System.Drawing.Color.DimGray;
+            this.pnlTitulo.BackColor = System.Drawing.SystemColors.HotTrack;
             this.pnlTitulo.Controls.Add(this.pbxCerrar);
             this.pnlTitulo.Controls.Add(this.lblTitulo);
             this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
@@ -51,6 +58,17 @@
             this.pnlTitulo.Name = "pnlTitulo";
             this.pnlTitulo.Size = new System.Drawing.Size(854, 35);
             this.pnlTitulo.TabIndex = 0;
+            // 
+            // pbxCerrar
+            // 
+            this.pbxCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxCerrar.BackgroundImage = global::ORM.Properties.Resources.Cerrar;
+            this.pbxCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxCerrar.Location = new System.Drawing.Point(817, 0);
+            this.pbxCerrar.Name = "pbxCerrar";
+            this.pbxCerrar.Size = new System.Drawing.Size(37, 35);
+            this.pbxCerrar.TabIndex = 1;
+            this.pbxCerrar.TabStop = false;
             // 
             // lblTitulo
             // 
@@ -64,19 +82,15 @@
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "ORM";
             // 
-            // pbxCerrar
-            // 
-            this.pbxCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pbxCerrar.BackgroundImage = global::ORM.Properties.Resources.Cerrar;
-            this.pbxCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pbxCerrar.Location = new System.Drawing.Point(817, 0);
-            this.pbxCerrar.Name = "pbxCerrar";
-            this.pbxCerrar.Size = new System.Drawing.Size(37, 35);
-            this.pbxCerrar.TabIndex = 1;
-            this.pbxCerrar.TabStop = false;
-            // 
             // pnlContenido
             // 
+            this.pnlContenido.Controls.Add(this.btnEntrar);
+            this.pnlContenido.Controls.Add(this.txtDatabase);
+            this.pnlContenido.Controls.Add(this.lblDatabase);
+            this.pnlContenido.Controls.Add(this.txtPwd);
+            this.pnlContenido.Controls.Add(this.lblPwd);
+            this.pnlContenido.Controls.Add(this.txtPuerto);
+            this.pnlContenido.Controls.Add(this.lblPuerto);
             this.pnlContenido.Controls.Add(this.txtUser);
             this.pnlContenido.Controls.Add(this.lblUser);
             this.pnlContenido.Controls.Add(this.lblServer);
@@ -84,16 +98,25 @@
             this.pnlContenido.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContenido.Location = new System.Drawing.Point(0, 35);
             this.pnlContenido.Name = "pnlContenido";
-            this.pnlContenido.Size = new System.Drawing.Size(854, 502);
+            this.pnlContenido.Size = new System.Drawing.Size(854, 474);
             this.pnlContenido.TabIndex = 1;
             // 
-            // cbxServer
+            // txtUser
             // 
-            this.cbxServer.FormattingEnabled = true;
-            this.cbxServer.Location = new System.Drawing.Point(147, 25);
-            this.cbxServer.Name = "cbxServer";
-            this.cbxServer.Size = new System.Drawing.Size(159, 29);
-            this.cbxServer.TabIndex = 0;
+            this.txtUser.Location = new System.Drawing.Point(148, 75);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(158, 27);
+            this.txtUser.TabIndex = 3;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblUser.Location = new System.Drawing.Point(92, 78);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(50, 21);
+            this.lblUser.TabIndex = 2;
+            this.lblUser.Text = "User: ";
             // 
             // lblServer
             // 
@@ -105,35 +128,86 @@
             this.lblServer.TabIndex = 1;
             this.lblServer.Text = "Choose Server: ";
             // 
-            // lblUser
+            // cbxServer
             // 
-            this.lblUser.AutoSize = true;
-            this.lblUser.BackColor = System.Drawing.Color.Transparent;
-            this.lblUser.Location = new System.Drawing.Point(12, 99);
-            this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(50, 21);
-            this.lblUser.TabIndex = 2;
-            this.lblUser.Text = "User: ";
+            this.cbxServer.FormattingEnabled = true;
+            this.cbxServer.Location = new System.Drawing.Point(147, 25);
+            this.cbxServer.Name = "cbxServer";
+            this.cbxServer.Size = new System.Drawing.Size(159, 29);
+            this.cbxServer.TabIndex = 0;
             // 
-            // txtUser
+            // lblPuerto
             // 
-            this.txtUser.Location = new System.Drawing.Point(68, 96);
-            this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(100, 27);
-            this.txtUser.TabIndex = 3;
+            this.lblPuerto.AutoSize = true;
+            this.lblPuerto.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuerto.Location = new System.Drawing.Point(92, 126);
+            this.lblPuerto.Name = "lblPuerto";
+            this.lblPuerto.Size = new System.Drawing.Size(49, 21);
+            this.lblPuerto.TabIndex = 4;
+            this.lblPuerto.Text = "Port: ";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(147, 123);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(159, 27);
+            this.txtPuerto.TabIndex = 5;
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.BackColor = System.Drawing.Color.Transparent;
+            this.lblPwd.Location = new System.Drawing.Point(51, 169);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(90, 21);
+            this.lblPwd.TabIndex = 6;
+            this.lblPwd.Text = "Password: ";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(147, 166);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(159, 27);
+            this.txtPwd.TabIndex = 7;
+            // 
+            // lblDatabase
+            // 
+            this.lblDatabase.AutoSize = true;
+            this.lblDatabase.BackColor = System.Drawing.Color.Transparent;
+            this.lblDatabase.Location = new System.Drawing.Point(44, 223);
+            this.lblDatabase.Name = "lblDatabase";
+            this.lblDatabase.Size = new System.Drawing.Size(97, 21);
+            this.lblDatabase.TabIndex = 8;
+            this.lblDatabase.Text = "Database: ";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(147, 220);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(159, 27);
+            this.txtDatabase.TabIndex = 9;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(147, 270);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(159, 34);
+            this.btnEntrar.TabIndex = 10;
+            this.btnEntrar.Text = "Go";
+            this.btnEntrar.UseVisualStyleBackColor = true;
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
-            this.ClientSize = new System.Drawing.Size(854, 537);
+            this.ClientSize = new System.Drawing.Size(854, 509);
             this.Controls.Add(this.pnlContenido);
             this.Controls.Add(this.pnlTitulo);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.frmLogin_Load);
@@ -156,6 +230,13 @@
         private System.Windows.Forms.Label lblServer;
         private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label lblPuerto;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.Button btnEntrar;
     }
 }
 
