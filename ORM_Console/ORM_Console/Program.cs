2528736a0ca4f4a8076ce624867481ee69f954db
Eliott Ivan Salazar Quiajda <eliottsalazar@gmail.com>
2018-10-18 16:57:06 -0700

    .

---
+++ ORM_Console/ORM_Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibBase_Datos;
+
+namespace ORM_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuarios users = new Usuarios();
+            users.Name = "Eliott";
+            users.Apellido = "Salazar";
+            users.Correo = "eliottsalazar@gmail.com";
+            users.Telefono = "6622814620";
+            users.Rfc = "kjdaiosdhiash23";
+            string format = users.Telefono + "Incorrecto";
+            Console.ReadKey();
+        }
+    }
+}
