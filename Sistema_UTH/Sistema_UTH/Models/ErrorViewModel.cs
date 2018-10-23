84fff42ea144772ead4d185f763ba575f0a231dc
Eliott Ivan Salazar Quiajda <eliottsalazar@gmail.com>
2018-10-22 18:51:47 -0700

    .

---
+++ Sistema_UTH/Sistema_UTH/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema_UTH.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
