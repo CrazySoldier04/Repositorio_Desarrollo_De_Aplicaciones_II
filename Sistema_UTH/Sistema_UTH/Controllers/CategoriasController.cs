e4fa049e0ecd4a2d2a508f27e7b17576ad433f5c
Eliott Ivan Salazar Quiajda <eliottsalazar@gmail.com>
2018-10-25 16:56:14 -0700

    Hacer módulo de estudiantes para el lunes.

--- Sistema_UTH/Sistema_UTH/Controllers/CategoriasController.cs
+++ Sistema_UTH/Sistema_UTH/Controllers/CategoriasController.cs
@@ -19,9 +19,37 @@ namespace Sistema_UTH.Controllers
         }
 
         // GET: Categorias
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
         {
-            return View(await _context.Categoria.ToListAsync());
+            ViewData["NombreSortParm"] = String.IsNullOrEmpty(sortOrder) ? "nombre_desc" : "";
+            ViewData["DescripcionSortParm"] = sortOrder == "descripcion_asc" ? "descripcion_desc" : "descripcion_asc";
+            ViewData["CurrentFilter"] = searchString;
+            var categorias = from s in _context.Categoria select s;
+
+            //Verifica que el término de búsqueda trae searchstring si trae alguno, y verificar si contiene un nombre o descripcion.
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                categorias = categorias.Where(s => s.Nombre.Contains(searchString) || s.Descripcion.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "nombre_desc":
+                    categorias = categorias.OrderByDescending(s => s.Nombre);
+                    break;
+                case "descripcion_asc":
+                    categorias = categorias.OrderBy(s => s.Descripcion);
+                    break;
+                case "descripcion_desc":
+                    categorias = categorias.OrderByDescending(s => s.Descripcion);
+                    break;
+                default:
+                    categorias = categorias.OrderBy(s => s.Nombre);
+                    break;
+            }
+            //return View(await _context.Categoria.ToListAsync());
+            //Regresa la vista con el ordenamiento realizado a la colección categorías.
+            return View(await categorias.AsNoTracking().ToListAsync());
         }
 
         // GET: Categorias/Details/5
