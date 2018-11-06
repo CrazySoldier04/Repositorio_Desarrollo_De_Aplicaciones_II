48112c927d2ddd1969cc6164f97c3d584026bb04
Eliott Ivan Salazar Quiajda <eliottsalazar@gmail.com>
2018-11-05 19:47:06 -0800

    .

---
+++ Sistema_UTH/Sistema_UTH/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_UTH.Data;
+
+namespace Sistema_UTH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> user;
+    }
+}
