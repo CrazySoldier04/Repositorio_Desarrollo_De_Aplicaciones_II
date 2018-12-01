ca70460e5ca71955cb61f083bf8e0ef2b2c3b008
Eliott Ivan Salazar Quiajda <eliottsalazar@gmail.com>
2018-12-01 11:46:16 -0700

    .

--- Threads_Bombas/Threads_Bombas/Program.cs
+++ Threads_Bombas/Threads_Bombas/Program.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threads_Bombas
 {
     class Program
     {
+        static Semaphore s = new Semaphore(1, 2);
         static void Main(string[] args)
         {
-
+            for (int i = 1; i < 6; i++)
+            {
+                new Thread(Program.Metodo).Start(i);
+            }
+            Console.ReadKey();
+        }
+        static void Metodo(object id)
+        {
+            Console.WriteLine("El carro: " + id + " quiere echar gasolina");
+            s.WaitOne();
+            Console.WriteLine("El carro: " + id + " entró a la bomba de gasolina");
+            Thread.Sleep(1000);
+            Console.WriteLine("Estado: {0}", new ThreadState());
+            Console.WriteLine("El carro: " + id + " salió de la bomba");
+            s.Release();
+            Thread.Sleep(1000);
+            Console.WriteLine("Estado: {0}", new ThreadState());
         }
     }
 }
